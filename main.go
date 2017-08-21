56260dc5d0485a1ed0c8484df0f08bc0136ac3c2
IamBusy <1342247033@qq.com>
2017-08-21 15:53:05 +0800

    Resolve ptr parameter

--- main.go
+++ main.go
@@ -18,8 +18,12 @@ func Collection(entities interface{}, transformerName string, includeStr string,
 	transformer := app.MustGet(PREFIX + transformerName).(Transformer)
 	v := reflect.ValueOf(entities)
 	var res []interface{}
-	if v.Kind() != reflect.Slice {
-		panic("entities should be slice, but receive:" + v.Kind().String())
+	if v.Kind() != reflect.Slice && v.Kind() != reflect.Array {
+		if v.Kind() == reflect.Ptr {
+			v = v.Elem()
+		} else {
+			panic("entities should be slice, but receive:" + v.Kind().String())
+		}
 	}
 	for i := 0; i < v.Len(); i++ {
 		res = append(res, transformer.Apply(v.Index(i).Interface(), includeStr, args))
